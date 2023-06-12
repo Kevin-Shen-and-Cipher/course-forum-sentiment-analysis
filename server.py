f2f96ef8c9bdf104f037ec3103dfeb6af783b126
Jerry <c109151150@nkust.edu.tw>
2023-06-13 04:19:36 +0800

    feature/sentiment analysis complete

--- server.py
+++ server.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from utils import config, sentiment_analysis
 
 app = Flask(__name__)
+_IP = config.get_ip()
+_PORT = config.get_port()
+
+@app.route('/get_emotional_score', methods=['POST'])
+def get_emotional_score():
+    content = request.get_json()
+    try:
+        text = content['text']
+        http_code, data = sentiment_analysis.get_emotional_score(text)
+        return jsonify(data), http_code
+    except KeyError as error:
+        error_message = f"Missing required key: {error}"
+        return jsonify({'error': error_message}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host=_IP, port=_PORT)
